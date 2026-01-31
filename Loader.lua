21892b8e1d6bf88840ea717aab41737ca47e558c
DarkMcSlark <thinkagain11109@gmail.com>
2026-01-31 13:06:36 -0700

    Clarify Operation One URL in Loader.lua
    Added comment to clarify the purpose of the URL in the Place_Id_To_Loadstring table.

--- Loader.lua
+++ Loader.lua
@@ -2,7 +2,7 @@ local Players = cloneref(game:GetService("Players"))
 local LocalPlayer = Players.LocalPlayer
 
 local Place_Id_To_Loadstring = {
-    [72920620366355] = "https://raw.githubusercontent.com/DarkMcSlark/DarkizzModule/refs/heads/main/OperationOne.lua"
+    [72920620366355] = "https://raw.githubusercontent.com/DarkMcSlark/DarkizzModule/refs/heads/main/OperationOne.lua"; -- // Operation One
 }
 
 if not Place_Id_To_Loadstring[game.PlaceId] then
